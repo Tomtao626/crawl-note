1726d595f9af121cb166d89af267a04fe1bc3d9a
Tomtao626 <tp320670258@gmail.com>
2022-10-19 17:26:12 +0800

    bugfix

--- basic/xpath_demo/dangjian_demo.py
+++ basic/xpath_demo/dangjian_demo.py
@@ -15,6 +15,7 @@ def GetHomeLinks(url, headers):
     HomepageLinks = []
     r = requests.get(url, headers=headers)
     html = etree.HTML(r.text)
+    print(html)
     HomeLinks = html.xpath('//div[@class="main-left"]/ul/li/div/a/@href')
     for HomeLink in HomeLinks:
         htmlPage = 'http://dangjian.com/djw2016sy/djw2016wkztl/wkztl2016xihy' + str(HomeLink)[1:]
@@ -43,5 +44,5 @@ if __name__ == '__main__':
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36',
         'Cookie': 'wdcid=7c80b781c03f1605; wdlast=1583386171'
     }
-    url = "http://dangjian.com/djw2016sy/djw2016wkztl/wkztl2016xihy/index.shtml"
+    url = "http://dangjian.com/djw2022sy/djw2016wkztl/wkztl2022xihy/index.shtml"
     DownloadPage(GetHomeLinks(url, headers), headers)
